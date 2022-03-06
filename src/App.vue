fea154c32fa0ebfbb0c84c00822d139c9b84e3d7
HanLiang <ihan_liang@163.com>
2022-03-06 16:17:52 +0800

    123

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,6 @@
 <template>
    <!-- minimal loader shown until image descriptors are loaded. Loading may take a while according to the device computational power -->
-   <div class="arjs-loader">
-    <div>Loading, please wait...</div>
-  </div>
 
-  <!-- a-frame scene -->
-  <a-scene
-    vr-mode-ui="enabled: false;"
-    renderer="logarithmicDepthBuffer: true;"
-    embedded
-    arjs="trackingMethod: best; sourceType: webcam;debugUIEnabled: false;"
-  >
-    <!-- a-nft is the anchor that defines an Image Tracking entity -->
-    <!-- on 'url' use the path to the Image Descriptors created before. -->
-    <!-- the path should end with the name without the extension e.g. if file is 'pinball.fset' the path should end with 'pinball' -->
-    <a-nft
-      type="nft"
-      url="dist/trex/trex-image/all"
-      smooth="true"
-      smoothCount="10"
-      smoothTolerance=".01"
-      smoothThreshold="5"
-    >
-      <!-- as a child of the a-nft entity, you can define the content to show. here's a GLTF model entity -->
-      <a-entity
-        gltf-model="./trex/scene.gltf"
-        scale="5 5 5"
-        position="50 150 0"
-      >
-      </a-entity>
-    </a-nft>
-    <!-- static camera that moves according to the device movemenents -->
-    <a-entity camera></a-entity>
-  </a-scene>
 </template>
 
 <script setup>

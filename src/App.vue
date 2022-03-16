2f02f0d8c838e1fe5255441afc4fa8c55d4e0a58
HanLiang <ihan_liang@163.com>
2022-03-17 00:31:29 +0800

    ch4

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
 <template>
    <!-- minimal loader shown until image descriptors are loaded. Loading may take a while according to the device computational power -->
+    <a-scene>
+      
+      <a-sky color="#ECECEC"></a-sky>
+      <MyAtom />
+    </a-scene>
 
 </template>
 
@@ -7,25 +12,9 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from './components/HelloWorld.vue'
+import MyAtom from './components/MyAtom.vue'
 </script>
 
 <style>
-  .arjs-loader {
-    height: 100%;
-    width: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    background-color: rgba(0, 0, 0, 0.8);
-    z-index: 9999;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-  }
 
-  .arjs-loader div {
-    text-align: center;
-    font-size: 1.25em;
-    color: white;
-  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import AFRAME from 'aframe';
+const app = createApp(App);
+app.use(AFRAME);
+app.mount('#app')
 
-createApp(App).mount('#app')
+
+AFRAME.registerPrimitive('a-atom', {
+    // Attaches the `ocean` component by default.
+    // Defaults the ocean to be parallel to the ground.
+    defaultComponents: {
+        atom: {},
+        geometry: { primitive: 'sphere' }
+    },
+
+    // Maps HTML attributes to the `ocean` component's properties.
+    mappings: {
+        width: 'atom.width',
+        depth: 'atom.depth',
+        density: 'atom.density',
+        color: 'atom.color',
+        opacity: 'atom.opacity'
+    }
+});

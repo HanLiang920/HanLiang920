9d4054a2bc9dfdd63fddc0a372c66e0329af2479
HanLiang <ihan_liang@163.com>
2022-03-23 21:03:01 +0800

    init

--- src/components/BaseBond.vue
+++ src/components/BaseBond.vue
@@ -20,16 +20,28 @@ const bondInfo = computed(() => {
     const { x: x2, y: y2, z: z2 } = bondAtom2
     const r1 = bondAtom1Info.value.radius
     const r2 = bondAtom2Info.value.radius
-    const x = (x1 + x2 + r1 - r2) / 2,
-        y = (y1 + y2 + r1 - r2) / 2,
-        z = (z1 + z2 + r1 - r2) / 2
+    const x = (x1 + x2) / 2,
+        y = (y1 + y2) / 2,
+        z = (z1 + z2) / 2,
+        length = Math.pow(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2) + Math.pow((z1 - z2), 2), 0.5)
+    
     return {
         position: `${x} ${y} ${z}`,
-        length: Math.pow(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2) + Math.pow((z1 - z2), 2), 0.5) - r1 - r2,
-        rotation: `${Math.atan((y - y1) / (x - x1)) * 180 / Math.PI} 0 ${Math.atan((y - y1) / (z - z1)) * 180 / Math.PI}`
+        length,
+        rotation: `${getAngle([z1, y1], [z, y], Math.pow(Math.pow((z1 - z2), 2) + Math.pow((y1 - y2), 2), 0.5) / 2)} ${getAngle([z1, x1], [z, x], Math.pow(Math.pow((z1 - z2), 2) + Math.pow((x1 - x2), 2), 0.5) / 2) - 180} ${getAngle([x1, y1], [x, y], Math.pow(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2), 0.5) / 2)}`
     }
 });
 
+
+function getAngle([x1, y1], [x2, y2], r) {
+    console.log(Math.pow(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2), 0.5),[x1, y1], [x2, y2],r);
+    if (x1 === x2) return 0
+    y2 = y2 - r
+    const length = Math.pow(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2), 0.5) / 2
+    const angle = Math.asin(length / r) * 180 / Math.PI *2
+    console.log(angle);
+    return x1 > x2 ? angle : -angle
+}
 
 </script>
  

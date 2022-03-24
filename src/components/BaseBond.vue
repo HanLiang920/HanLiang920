3b0214e80bb275d8bef0f9ba212de6d97b6add68
HanLiang <ihan_liang@163.com>
2022-03-25 01:07:14 +0800

    难着不会，会着不难

--- src/components/BaseBond.vue
+++ src/components/BaseBond.vue
@@ -23,31 +23,34 @@ const bondInfo = computed(() => {
     const x = (x1 + x2) / 2,
         y = (y1 + y2) / 2,
         z = (z1 + z2) / 2,
-        length = Math.pow(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2) + Math.pow((z1 - z2), 2), 0.5),
-        length2 = Math.pow(Math.pow((x1 - x), 2) + Math.pow((y1 - y), 2) + Math.pow((z1 - z), 2), 0.5)
-     // 270 270 30 330
+        length = Math.pow(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2) + Math.pow((z1 - z2), 2), 0.5)
+
     return {
         position: `${x} ${y} ${z}`,
         length,
-        rotation: `0 ${getAngle2([x,z], [x1,z1], length2)} ${getAngle2([x,y], [x1,y1], length2)}`
+        rotation: getRotation([x, y, z], [x1, y1, z1])
     }
 });
 
-
-function getAngle2([x1,y1],[x2, y2] , r) {
-    const angle = Math.asin((y2 - y1) / r) * 180 / Math.PI
-    if(x2>=x1) return 90 +  angle
-    return 270 - angle
-    
+function getRotation([x, y, z], [x1, y1, z1]) {
+    const length = Math.pow(Math.pow((x1 - x), 2) + Math.pow((y1 - y), 2) + Math.pow((z1 - z), 2), 0.5)
+    const roll = Math.asin((y1 - y) / length) * 180 / Math.PI + 90
+    const length2 = Math.pow(Math.pow((x1 - x), 2) + Math.pow((z1 - z), 2), 0.5)
+    let yaw = Math.asin((z - z1) / length2) * 180 / Math.PI
+    if (x >= x1) {
+        yaw = 180 - yaw
+    }
+    return `0 ${yaw} ${roll}`
 }
 
+
 function getAngle([x1, y1], [x2, y2], r) {
-    console.log(Math.pow(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2), 0.5),[x1, y1], [x2, y2],r);
+    console.log(Math.pow(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2), 0.5), [x1, y1], [x2, y2], r);
     y2 = y2 - r
     if (y1 === y2) return 0
     x1 = x2 + r
     const length = Math.pow(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2), 0.5) / 2
-    const angle = Math.asin(length / r) * 180 / Math.PI *2
+    const angle = Math.asin(length / r) * 180 / Math.PI * 2
     console.log(angle);
     return x1 > x2 ? angle : -angle
 }
@@ -59,20 +62,9 @@ function getAngle([x1, y1], [x2, y2], r) {
         :position="bondInfo.position"
         :scale="`1 ${bondInfo.length / 0.2} 1`"
         :rotation="bondInfo.rotation"
-        
     >
-        <a-cylinder
-            :color="bondAtom1Info.color"
-            :height="0.1"
-            radius="0.05"
-            position="0 -0.05 0"
-        ></a-cylinder>
-        <a-cylinder
-            :color="bondAtom2Info.color"
-            :height="0.1"
-            radius="0.05"
-            position="0 0.05 0"
-        ></a-cylinder>
+        <a-cylinder :color="bondAtom1Info.color" height="0.1" radius="0.05" position="0 -0.05 0"></a-cylinder>
+        <a-cylinder :color="bondAtom2Info.color" height="0.1" radius="0.05" position="0 0.05 0"></a-cylinder>
     </a-entity>
 </template>
 

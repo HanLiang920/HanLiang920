52a0ce41a426418204fb188224aa9d173461098a
韩亮 <ihan_liang@163.com>
2022-03-30 20:56:06 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -3,11 +3,11 @@
     <a-scene>
       <a-sky color="#ECECEC"></a-sky>
       <!-- <fragment type="甲烷" position="0 0 0"/> -->
-      <fragment type="乙烷" position="0 0 0"/>
-      <fragment type="乙烯" position="6 0 0"/>
+      <fragment type="乙烷" position="-4 0 0"/>
+      <fragment type="乙烯" position="0 0 0"/>
       <fragment type="乙炔" position="6 0 0"/>
-      <fragment type="苯" position="8 0 0"/>
-      
+      <fragment type="苯" position="10 0 0"/>
+      <BaseCamera />
     </a-scene>
 
 </template>
@@ -16,6 +16,7 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import fragment from './components/fragment.vue'
+import BaseCamera from './components/BaseCamera.vue'
 </script>
 
 <style>
---
+++ src/components/BaseCamera.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref,onMounted } from 'vue'
+const camera = ref(null)
+onMounted(() => {
+    setTimeout(()=>{
+        console.log(camera.value.object3DMap.camera);
+        camera.value.object3DMap.camera.position.set(5, 0, 10);
+    },0)
+   
+})
+</script>
+
+<template>
+  <a-camera ref='camera'></a-camera>
+</template>

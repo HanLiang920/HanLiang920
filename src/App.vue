152f0d0543e6458980979922d9c97bf7a5ebd52d
韩亮 <ihan_liang@163.com>
2022-04-19 22:09:33 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -6,17 +6,13 @@
   </div>
   <div class="chem-3d-content">
     <a-scene embedded arjs="sourceType: webcam; debugUIEnabled: false; detectionMode: mono_and_matrix; matrixCodeType: 4x4_BCH_13_9_3;" vr-mode-ui="enabled: false">
-      <a-marker type="barcode" value="0">
-        <fragment type="甲烷" :key="currentModel[0]"/>
-        <!-- <fragment type="乙烷"  />
-        <fragment type="乙烯" position="0 0 0" />
-        <fragment type="乙炔" position="6 0 0" />
-        <fragment type="苯" position="10 0 0" /> -->
+      <a-marker type="barcode" value="0" smooth="true">
+        <fragment type="甲烷"/>
       </a-marker>
-        <a-marker type="barcode" value="1">
-        <fragment type="乙烷" :key="currentModel[0]"/>
+      <a-marker type="barcode" value="1" smooth="true">
+        <fragment type="乙烷"/>
       </a-marker>
-      <BaseCamera />
+       <a-entity camera></a-entity>
     </a-scene>
   </div>
   <a-drawer

875cf1016830ed4ca63aec0a034738a3bb857abc
韩亮 <929330116@qq.com>
2022-08-12 09:58:24 +0800

    修改切换动画

--- src/components/modeChoose.vue
+++ src/components/modeChoose.vue
@@ -7,7 +7,7 @@ defineEmits(['update:mode'])
 </script>
 
 <template>
-    <div class="mode-choose">
+    <div class="mode-choose" :style="{'--groove-left':mode !== 'AR'?'0':'50%'}">
         <div :class="{ 'mode-choose-item': true, choose: mode != 'AR' }" @click="$emit('update:mode', '模型')">模型</div>
         <div :class="{ 'mode-choose-item': true, choose: mode == 'AR' }" @click="$emit('update:mode', 'AR')">AR</div>
     </div>
@@ -18,6 +18,19 @@ defineEmits(['update:mode'])
     width: 45%;
     display: flex;
     font-size: 14px;
+    position: relative;
+    &::before {
+        content: "";
+        position: absolute;
+        left: var(--groove-left);
+        width: 50%;
+        height: 100%;
+        border-radius: 15px;
+        transition: left 0.6s cubic-bezier(.17,.67,.83,.67);
+        background: #141414;
+        opacity: 0.8;
+        z-index: 1;
+    }
 
     .mode-choose-item {
         width: 50%;
@@ -27,10 +40,9 @@ defineEmits(['update:mode'])
         display: flex;
         align-items: center;
         justify-content: center;
-
+        z-index: 2;
+        transition: color 0.3s linear;
         &.choose {
-            background: #141414;
-            border-radius: 15px;
             color:#1890ff;
         }
     }

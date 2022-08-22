e3c90461341ffde68206c849cba785a9322c07a5
HanLiang <ihan_liang@163.com>
2022-08-22 14:53:20 +0800

    更新电子对

--- src/components/BaseElectronPair.vue
+++ src/components/BaseElectronPair.vue
@@ -24,8 +24,8 @@ const position = computed(() => {
   <a-entity :position="position" :rotation="rotation" scale="0.6 0.6 0.6" >
     <a-entity :key="id" position="0 -1.91 0" geometry="primitive: electronPair;" material="color: yellow;roughness: 0.25; metalness: 0.2;opacity:0.5"></a-entity>
     <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 10000;easing: linear">
-      <a-entity geometry="primitive: sphere; radius: 0.13" material="color: red;roughness: 0.25; metalness: 0.2" position="0.3 -1.25 0"></a-entity>
-      <a-entity geometry="primitive: sphere; radius: 0.13" material="color: red;roughness: 0.25; metalness: 0.2" position="-0.3 -1.25 0"></a-entity>
+      <a-sphere position="0.3 -1.25 0" color="red" radius="0.13" roughness="0.25" metalness="0.2"></a-sphere>
+      <a-sphere position="-0.3 -1.25 0" color="red" radius="0.13" roughness="0.25" metalness="0.2"></a-sphere>
     </a-entity>
   </a-entity>
   

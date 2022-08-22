00800b3cb0c0f111c1e63e628dc70cd403aa66b4
HanLiang <ihan_liang@163.com>
2022-08-22 18:19:22 +0800

    1

--- src/components/BaseElectronPair.vue
+++ src/components/BaseElectronPair.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, onMounted } from "vue";
 import { getRotation } from '../utils'
 const { bondAtom1, bondAtom2,id} = defineProps({
     bondAtom1: Object,
@@ -18,18 +18,21 @@ const position = computed(() => {
     return `${x} ${y} ${z}`
 });
 
+const sphere1 = ref()
+const sphere2 = ref()
+onMounted(() => {
+  sphere1.value.object3D.position.set(0.3, -1.25, 0);
+  sphere2.value.object3D.position.set(-0.3, -1.25, 0);
+})
+
 </script>
 
 <template>
   <a-entity :position="position" :rotation="rotation" scale="0.6 0.6 0.6" >
     <a-entity :key="id" position="0 -1.91 0" geometry="primitive: electronPair;" material="color: yellow;roughness: 0.25; metalness: 0.2;opacity:0.5"></a-entity>
-    <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 10000; easing: linear; delay: 10">
-      <a-entity position="0 0 0" animation="property: position; to: 0.3 -1.25 0; dur: 10;">
-        <a-sphere position="0 0 0" color="red" radius="0.13" roughness="0.25" metalness="0.2"></a-sphere>
-      </a-entity>
-      <a-entity position="0 0 0" animation="property: position; to: -0.3 -1.25 0; dur: 10;">
-        <a-sphere position="0 0 0" color="red" radius="0.13" roughness="0.25" metalness="0.2"></a-sphere>
-      </a-entity>
+    <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 10000;easing: linear">
+      <a-sphere ref="sphere1" color="red" radius="0.13" roughness="0.25" metalness="0.2"></a-sphere>
+      <a-sphere ref="sphere2" color="red" radius="0.13" roughness="0.25" metalness="0.2"></a-sphere>
     </a-entity>
   </a-entity>
   

fcd82701816413a08becd6ea89a8a90a35dfc6c4
HanLiang <ihan_liang@163.com>
2022-08-22 16:16:03 +0800

    修改key

--- src/components/fragment.vue
+++ src/components/fragment.vue
@@ -19,17 +19,17 @@ const fragment = reactive(window.c3Data[type]);
 <template>
 <a-entity use-rotate v-if="fragment">
   <a-entity shadow  :position="fragment.center" :rotation="fragment.rotation">
-    <template  v-for="atom in fragment.atom" :key="atom.id">
-      <BaseAtom v-if="atom.symbol" v-bind="atom"/>
+    <template  v-for="atom in fragment.atom">
+      <BaseAtom v-if="atom.symbol" v-bind="atom" :key="atom.id"/>
     </template>
-    <template v-for="bond in fragment.bond" :key="bond.id">
-      <BaseElectronPair v-if="bond.isElectronPair===true" :bondAtom1="fragment.atom[bond.bondAtom1]" :bondAtom2="fragment.atom[bond.bondAtom2]" :id="type+bond.id"/>
-      <BaseZahua v-else-if="bond.zahua===true" :height="bond.height" :scale="bond.scale" :opacity="bond.opacity" :bondAtom1="fragment.atom[bond.bondAtom1]" :bondAtom2="fragment.atom[bond.bondAtom2]" :id="type+bond.id"/> 
-      <BaseSS v-else-if="bond.ss===true" :height="bond.height" :scale="bond.scale" :opacity="bond.opacity" :bondAtom1="fragment.atom[bond.bondAtom1]" :bondAtom2="fragment.atom[bond.bondAtom2]" :id="type+bond.id"/>
-      <BaseSP v-else-if="bond.sp===true" :height="bond.height" :scale="bond.scale" :opacity="bond.opacity" :bondAtom1="fragment.atom[bond.bondAtom1]" :bondAtom2="fragment.atom[bond.bondAtom2]" :id="type+bond.id"/>
-      <BasePPσ v-else-if="bond.ppσ===true" :height="bond.height" :scale="bond.scale" :opacity="bond.opacity" :bondAtom1="fragment.atom[bond.bondAtom1]" :bondAtom2="fragment.atom[bond.bondAtom2]" :id="type+bond.id"/>
+    <template v-for="bond in fragment.bond">
+      <BaseElectronPair v-if="bond.isElectronPair===true" :bondAtom1="fragment.atom[bond.bondAtom1]" :bondAtom2="fragment.atom[bond.bondAtom2]" :id="type+bond.id" :key="bond.id"/>
+      <BaseZahua v-else-if="bond.zahua===true" :height="bond.height" :scale="bond.scale" :opacity="bond.opacity" :bondAtom1="fragment.atom[bond.bondAtom1]" :bondAtom2="fragment.atom[bond.bondAtom2]" :id="type+bond.id" :key="bond.id"/> 
+      <BaseSS v-else-if="bond.ss===true" :height="bond.height" :scale="bond.scale" :opacity="bond.opacity" :bondAtom1="fragment.atom[bond.bondAtom1]" :bondAtom2="fragment.atom[bond.bondAtom2]" :id="type+bond.id" :key="bond.id"/>
+      <BaseSP v-else-if="bond.sp===true" :height="bond.height" :scale="bond.scale" :opacity="bond.opacity" :bondAtom1="fragment.atom[bond.bondAtom1]" :bondAtom2="fragment.atom[bond.bondAtom2]" :id="type+bond.id" :key="bond.id"/>
+      <BasePPσ v-else-if="bond.ppσ===true" :height="bond.height" :scale="bond.scale" :opacity="bond.opacity" :bondAtom1="fragment.atom[bond.bondAtom1]" :bondAtom2="fragment.atom[bond.bondAtom2]" :id="type+bond.id" :key="bond.id"/>
       <BaseBond v-else :bondAtom1="fragment.atom[bond.bondAtom1]"
-        :bondAtom2="fragment.atom[bond.bondAtom2]" :bondOrderType="bond.bondOrderType" :bondOrder="bond.bondOrder" :color="bond.color"/>
+        :bondAtom2="fragment.atom[bond.bondAtom2]" :bondOrderType="bond.bondOrderType" :bondOrder="bond.bondOrder" :color="bond.color"  :key="bond.id"/>
     </template>
   </a-entity>
   <a-entity v-if="showAxes" axes-helper="size:2"></a-entity>
